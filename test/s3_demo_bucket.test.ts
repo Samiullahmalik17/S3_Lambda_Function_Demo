261ce7be7bc8fb01c051190d83fbac86f1297d9e
Samiullahmalik17 <samiullahmalk17@gmail.com>
2024-03-12 12:45:09 +0500

    Inital Commit

---
+++ test/s3_demo_bucket.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3DemoBucket from '../lib/s3_demo_bucket-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_demo_bucket-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3DemoBucket.S3DemoBucketStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
